6dcbd0dca9c1dad3765c0244637c26b8d0edd2f7
rd544 <roy10194464@live.missouristate.edu>
2019-04-26 12:53:52 -0500

    uwu

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -2,18 +2,24 @@
  * Roy Dawson	<roy10194464@live.missouristate.edu> 
  * @file		Main.cpp
  *
+ * Notes:       1. CMakeLists.txt file was copied from lab09
+ *              and edited to work for this homework.
+ *              2. Commented out sections were for if the
+ *              data was not in order so that it would sort
+ *              before running the simulation, this idea was
+ *              scrapped, as it was not required.
+ *
  */
 
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <vector>
+//#include <vector>
 #include <queue>
 #include "BankSim.h"
 
 int main(int argc, char* argv[]) {
-    std::cout << "Program start" << std::endl;
 
     std::ifstream data{"input.dat"};
     if (!data) {
@@ -21,44 +27,52 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
+    std::cout << "Day start." << std::endl;
+
     int enterTime;
     int processTime;
-	std::vector<BankSim> organize;
+//	std::vector<BankSim> organize;
     std::queue<BankSim> member;
 
+//    while (data >> enterTime >> processTime) {
+//		bool placed = false;
+//		BankSim bs{enterTime, processTime};
+//		if (organize.empty()) organize.push_back(bs);
+//		else{
+//			for (int i = 0; i < organize.size() && !placed; i++){
+//				if(bs.getEnterTime() < organize.at(i).getEnterTime()){
+//					organize.insert (i, bs);
+//					placed = true;
+//				}
+//
+//			}
+//			if (!placed) organize.push_back(bs);
+//		}
+//	}
+//	while (!organize.empty()){
+//		member.push(organize.front());
+//		organize.erase(organize.begin());
+//    }
+
     while (data >> enterTime >> processTime) {
-		bool placed = false;
-		BankSim bs{enterTime, processTime};
-		if (organize.empty()) organize.push_back(bs);
-		else{
-			for (int i = 0; i < organize.size() && !placed; i++){
-				if(bs.getEnterTime() < organize.at(i).getEnterTime){
-					organize.insert(i, bs);
-					placed = true;
-				}
-				
-			}
-			if (!placed) organize.push_back(bs);
-		}
-	}
-	while (!organize.empty()){
-		member.push(organize.front());
-		organize.erase(organize.begin());
+        BankSim bs{enterTime, processTime};
+        member.push(bs);
     }
 	
 	int time = member.front().getEnterTime();
 	
 	while (!member.empty()){
-		std::cout << "Service start at: " << time << std::endl;
-		int occupyTime = member.front().getProcessTime();
+        int occupyTime = 0;
+		std::cout << "Service start at: " << time << " minutes into day." << std::endl;
+		occupyTime = member.front().getProcessTime();
 		for (int i = 0; i < occupyTime; i++) time += 1;
 		member.pop();
-		std::cout << "Service ended at: " << time << std::endl;
+		std::cout << "Service ended at: " << time << " minutes into day." << std::endl;
 	}
 
     data.close();
 
-	std::cout << "Program end." << std::endl;
+	std::cout << "Day end." << std::endl;
 
     return EXIT_SUCCESS;
 }
